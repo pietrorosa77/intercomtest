9cfd534a6e5bb402c0fe62520cb98882493197b2
pietrorosa77 <pietrorosa77@gmail.com>
2017-03-14 23:38:15 +0000

    Update various.js

--- various.js
+++ various.js
@@ -7,3 +7,40 @@
     }
     return count;
 }*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+	public ListNode detectCycle(ListNode node) {
+	   
+	    ListNode slow = node;
+	    ListNode fast = node;
+	    boolean found = false;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // If slow and fast meet at same point then loop is present
+            if (slow == fast) {
+                found = true;
+                 break;
+            }
+        }
+        
+        if(found) {
+            slow = node;
+            while(fast != slow){
+                fast= fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+        return null;
+	}
+}
+
