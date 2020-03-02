8e9a0ccd0800f42ed0a0e159790e3158338ec3eb
Pietrorosa77 <pietro.rosa@auth0.com>
2020-03-02 09:29:14 +0100

    Update ds.js

--- ds.js
+++ ds.js
@@ -614,5 +614,53 @@ exports.Trie = function trie() {
 	}
 }
 
+public class Solution {
+	public ListNode detectCycle(ListNode node) {
+	   
+	    ListNode slow = node;
+	    ListNode fast = node;
+	    boolean found = false;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // If slow and fast meet at same point then loop is present
+            if (slow == fast) {
+                found = true;
+                 break;
+            }
+        }
+        
+        if(found) {
+            slow = node;
+            while(fast != slow){
+                fast= fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+        return null;
+	}
+}
+
+module.exports = { 
+	//param A : array of integers
+	//param B : integer
+	//return an integer
+	diffPossible : function(A, B){
+	    var size = A.length;
+	    var start = 0;
+	    var end =1;
+        while(start < size && end < size) {
+            if(end !== start && A[end] - A[start] === B){
+                return 1;
+            } else if (A[end] - A[start] < B)
+                end++;
+            else
+                start++;
+        }
+        return 0;
+	}
+};
+
 
 
